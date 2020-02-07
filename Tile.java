1df67c8525b72f309af9aed2506f6c62356f066c
Adam Caffrey <adamca99@gmail.com>
2020-02-07 12:02:47 +0000

    updating tile

--- Tile.java
+++ Tile.java
@@ -1,22 +1,29 @@
-class Tile (char letter, int value) 
-{
-private char letter 
-private int value 
+class Tile {
+	private char letter;
+	private int value;
+	
+	public Tile (char letter, int value) {
+		this.letter = letter;
+		this.value = value;
+	}
 
-public Tile (char letter, int value) {
- //[a-zA-Z]
-//char properValue = '[0-10]'
-//char isALetter = '[a-zA-Z]'
-	//check
-this.letter = letter
-this.value =  value;
+	public char readLetter ()
+	{ return letter; }
+	
+	public int readValue ()
+	{ return value; }
+	
+	public void display ()
+	{ System.out.println(letter + " " + value); }
+	
+	public String toString() {
+		return letter + "\n" + value;
+	}
+	
+	
+public static void main(String[] args) {
+		Tile t = new Tile('A', 1);
+		
+		System.out.print(t);
+	}
 }
-
-public char readLetter ()
-{ return letter; }
-
-public int readValue ()
-{ return value; }
-
-public void display ()
-{ System.out.printf(letter, value + "/t"); }
