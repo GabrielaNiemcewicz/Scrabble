8e859bbe7dded0da5fa2b25263275c415f0886d5
nagham aljarad <nagham.aljarad@ucdconnect.ie>
2020-02-07 13:00:45 +0000

    Fixing Tile

--- Tile.java
+++ Tile.java
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-class Tile (char letter, int value) 
-{
-private char letter;
-private int value;
-
-public Tile (char letter, int value) {
- //[a-zA-Z]
-//char properValue = '[0-10]'
-//char isALetter = '[a-zA-Z]'
-this.letter = letter;
-this.value =  value;
-}
-
-public char readLetter ()
-{ return letter; }
-
-public int readValue ()
-{ return value; }
-
-public void display ()
-{System.out.printf("%c%d",letter, value);}
-}
-=======
 class Tile {
 	private char letter;
 	private int value;
@@ -51,4 +27,3 @@ public static void main(String[] args) {
 		System.out.print(t);
 	}
 }
->>>>>>> refs/remotes/origin/master
