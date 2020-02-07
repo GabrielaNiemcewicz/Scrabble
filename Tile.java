7be50f13be37e1f698400bc7ec9af0f9ae117fe9
unknown <gabriela.niemcewicz@ucdconnect.ie>
2020-02-07 12:40:02 +0100

    Tile(value, letter), doesn't check inputs

--- Tile.java
+++ Tile.java
@@ -1,12 +1,15 @@
-class Tile (char letter) 
+class Tile (char letter, int value) 
 {
 private char letter 
 private int value 
 
-<<<<<<< HEAD
-public Tile (char letter) {
+public Tile (char letter, int value) {
+ //[a-zA-Z]
+//char properValue = '[0-10]'
+//char isALetter = '[a-zA-Z]'
+	//check
 this.letter = letter
-this.value =  setValue(letter);
+this.value =  value;
 }
 
 public char readLetter ()
@@ -16,98 +19,4 @@ public int readValue ()
 { return value; }
 
 public void display ()
-{ System.out.println(letter, value) }
-
-private int setValue(char letter){
-switch(letter) {
-case 'A':
-    return 1;
-    break;
-case 'B':
-    return 3;
-    break;
-case 'C':
-    return 3;
-    break;
-case 'D':
-    return 2;
-    break;
-case 'E':
-    return 1;
-    break;
-case 'F':
-    return 4;
-    break;
-case 'G':
-    return 2;
-    break;
-case 'H':
-    return 4;
-    break;
-case 'I':
-    return 1;
-    break;
-case 'J':
-    return 8;
-    break;
-case 'K':
-    return 5;
-    break;
-case 'L':
-    return 1;
-    break;
-case 'M':
-    return 3;
-    break;
-case 'N':
-    return 1;
-    break;
-case 'O':
-    return 1;
-    break;
-case 'P':
-    return 3;
-    break;
-case 'Q':
-    return 10;
-    break;
-case 'R':
-    return 1;
-    break;
-case 'S':
-    return 1;
-    break;
-case 'T':
-    return 1;
-    break;
-case 'U':
-    return 1;
-    break;
-case 'V':
-    return 4;
-    break;
-case 'W':
-    return 4;
-    break;
-case 'X':
-    return 8;
-    break;
-case 'Y':
-    return 4;
-    break;
-case 'Z':
-    return 10;
-    break;
-case ' ':
-    return 0;
-    break;
-
-default:
-throw new Exception("Such tile cannot be created");
-}
-}
-=======
-public class Tile {
-}
-
->>>>>>> c7e718b1fb25d21c9e0d9fabecbd0ef5bb578a3a
+{ System.out.printf(letter, value + "/t"); }
