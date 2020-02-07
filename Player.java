52cf5a2b430d31cc9b447b1bb2852754386cbc4d
nagham aljarad <nagham.aljarad@ucdconnect.ie>
2020-02-07 10:11:41 +0000

    Class Player created

---
+++ Player.java
@@ -0,0 +1,32 @@
+
+public class Player {
+	
+	private String Name;
+	private int Score;
+	
+	public Player(String name) {
+		
+		setName(name);
+	}
+	
+	public String getName() { return Name; }
+
+	public void setName(String name) {
+		/*This if statement checks for 3 different cases for the name:*/
+		if(name.length() == 0 /*if the user did not insert a name at all OR*/
+		|| " ".equals(name)   /*if the user inserted a white space character as a name OR*/
+		|| "\n".equals(name)) /*if the user hit a newline without entering a name*/
+			throw new IllegalArgumentException(); 
+		else
+		    Name = name;
+	}
+	
+	public int getScore() { return Score; }
+
+	public void setScore(int score) {
+		if(score < 0)             /*This if statement ensures that score 
+		                           * is always a positive integer. */
+			throw new IllegalArgumentException(); 
+		Score = score;
+	}
+}
