5d4ba0492bf53b55cc6d3507a45ce01a0f8c6b33
Adam Caffrey <adamca99@gmail.com>
2020-02-09 08:44:47 +0000

    Adding tests for pool

--- Main.java
+++ Main.java
@@ -6,9 +6,20 @@ public class Main {
 		Pool pool = new Pool();
 		
 		System.out.println("---------------------------------------------------------------------------------------------");
-		System.out.println("\t\t\tTesting Pool...");
+		System.out.println("\t\t\tTesting Pool...\n");
 		
-		System.out.println("Result: " + pool.IsEmpty());
+	//******************* Testing IsEmpty() ******************************
+		System.out.println("---Testing IsEmpty()---\n\n*When pool is full*");
+		System.out.println("Expected Result: False" + "\nResult: " + pool.IsEmpty());
+		
+		for(int i=0;i<20;i++)
+			pool.Draw(5);
+		System.out.println("\n*When pool is empty*");
+		System.out.println("Expected Result: True" + "\nResult: " + pool.IsEmpty());
+		
+		//******************* Testing resetPool() ******************************
+		pool.resetPool();
+		System.out.println("---Testing resetPool()---\n\n*When pool is empty*");
+		System.out.println("Expected Result: 0" + "\nResult: " + pool.NumOfTiles());
 	}
-
 }
