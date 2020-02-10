28f2a67bbafda0dcd90f4b812e2eb88ae48cd3ac
unknown <gabriela.niemcewicz@ucdconnect.ie>
2020-02-10 03:24:03 +0100

    Full unit tests plan for Frame. Tests try methods without action on fail

--- FrameTest.java
+++ FrameTest.java
@@ -24,7 +24,7 @@ class FrameTest {
 				assertTrue(frame.empty());
 				assertFalse(frame.full());
 				assertEquals(7, frame.requestTiles());
-				assertEquals("",frame.displayAllTilesAsLetters())
+				System.out.println("Expected output - nothing displayed, actual output:x",frame.displayAllTilesAsLetters())
 			} catch(IllegalArgumentException ex) {}
 			
 			try{
@@ -56,9 +56,12 @@ class FrameTest {
 			} catch(IllegalArgumentException ex) {}
 
 	}
-	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		Letter check_PassTiles() {
-			
+			try{
+			frame.passTiles()
+
+				}catch() {}
 			
 		}
 	
@@ -66,7 +69,10 @@ class FrameTest {
 	
 	
 		Letter check_removeTiles() {
-		
+		try{
+			frame.removeTiles()
+
+				}catch() {}
 		}
 	
 		Letter [] removeString(String passedString) //not created []- it's void now
@@ -79,19 +85,33 @@ class FrameTest {
 		
 		
 		public int check_atWhichIndex (char checkedLetter) {
-			
-			
+			try{
+			frame.atWhichIndex(checkedLetter)
+
+				}catch() {}
 		}
 		
 		
 		public boolean check_isStringIn(string passedString){
-			
+			try{
+			frame.isStringIn(passedString)
+
+				}catch() {}
 		}
 		
-		boolean check_empty()
+		boolean check_empty() {
+			try{
+			frame.Empty()
+
+				}catch() {}
+
+}
 		
 		ArrayList<Letter> accessAll() {
-			
+			try{
+			frame.accessAll()
+
+				}catch() {}
 			
 		}
 		
@@ -99,46 +119,110 @@ class FrameTest {
 		
 		
 		Letter check_accessTileByLetter (char checkedCharacter){
-			
+			try{
+			frame.accessTileByLetter(checkedLetter)
+
+				}catch() {}
 			
 			
 		}
 
 		Letter check_accessTileByIndex (int i) {
-			
+			try{
+			frame.accessTileByIndex(checkedLetter)
+
+				}catch() {}
 			
 		}
 		
 	
 		void check_addTiles (Tile[] letters) {
-			
+			try{
+			frame.addTiles(Tile [] letters)
+
+				}catch() {}
 			
 			
 			
 		}
 		
-		 boolean check_Full()
+		 boolean check_Full(){
+try{
+			frame.Full()
+
+				}catch() {}
+
+}
 		 
 		 public int requestTiles() {
-			 
+			 try{
+			frame.requestTiles()
+
+				}catch() {}
 			 
 		 }
 		 
 		 public void check_readAll()
 		 {
-		
+		try{
+			frame.readAll()
+
+				}catch() {}
 		 }
 
-		 public string check_displayAllTilesAsLetters()
+		 public string check_displayAllTilesAsLetters(){
+
+try{
+			frame.displayAllTilesAsLetters()
+
+				}catch() {}
+
 		 }
 	
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+Frame frame = new Frame ();
+			// tests on  empty frame //
+			Tile [] leters = new Tile[8] ;
+
+			Tile letterA = new Tile(a,2);
+			Tile letterB = new Tile(b,3);
+			Tile letterC = new Tile(c,4);
+			Tile letterD = new Tile(d,5);
+			Tile letterE = new Tile(e,6);
+			Tile letterF = new Tile(f,7);
+			Tile letterG = new Tile(g,8);
+			Tile letterH = new Tile(h,9);
+
+			letters[0] = letterA;
+			letters[1] = letterB;
+			letters[2] = letterC;
+			letters[3] = letterD;
+			letters[4] = letterE;
+			letters[5] = letterF;
+			letters[6] = letterG;
+			letters[7] = letterH;
+
+			//try adding each of letters to Frame, correctly display error at 8th Tile
+			//only addTiles([]) does it = test not exactly simulates Pool behaviour, 1 Tile at the time => check requestTiles() after each add 
+
+
+			//full frame tests/assertions
+
+			//test each non-removing non-adding non-requesting method on arbitrary number of Tiles, remove 1-3 casually
+
+			//test unused methods removing Tiles, including reset
+
+			//is frame empty after reset
