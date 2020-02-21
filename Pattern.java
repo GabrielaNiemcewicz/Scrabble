1b8cea8b3c8a71846d706f0f16c6d98edac8055d
Adam Caffrey <adamca99@gmail.com>
2020-02-21 10:14:03 +0000

    Assign rule generalization, no more double assign at (7,7). commented

--- Pattern.java
+++ Pattern.java
@@ -1,19 +1,24 @@
 //to draw valid Scrabble Board, use  use .drawScrabble. 
-//To use smaller methods, Board SIZE = 15, V argument size=7. use method. use print. use reset().
+//To use smaller methods, Board SIZE = 15, V argument size=7. Also, 7=middle() for SIZE=15. use method. use print. use reset().
  	//A = TRIPLE WORD SCORE
 	//B = DOUBLE WORD SCORE
 	//C= TRIPLE LETTER SCORE
 	//D= DOUBLE LETTER SCORE
 	//o= regular Square
 
+//algorithm uses 2 symmetry axes of scrabble board- one axis by divide and conquer, second by absolut= |x|
+//absolut = distance of referenced mirror images of 2 Squares from pointy "peak" of V. 
+
+
+
 public class Pattern {
-private char [][] cardboard; //Scrabble Board in chars
+public char [][] scrabble; //Scrabble Board in chars
 
 private int SIZE; //size of Board. For scrabble, SIZE=15
 
 Pattern(int size){ //initializer
 	this.SIZE = size;
-	this.setCardboard(new char[SIZE][SIZE]);
+	this.setScrabble(new char[SIZE][SIZE]);
 	this.reset(); //sets all characters to regular square, "o"
 
 }
@@ -21,7 +26,7 @@ Pattern(int size){ //initializer
 public void print() { //print board at any moment
 	for (int i=0; i<this.SIZE; i++)
 		for (int j=0; j<this.SIZE; j++) {
-			System.out.print(this.getCardboard()[i][j]+" "); //display each char one by one
+			System.out.print(this.getScrabble()[i][j]+" "); //display each char one by one
 			if (j==this.SIZE-1) 
 				System.out.println(""); //go down a line if the row is over
 			else continue;}
@@ -32,12 +37,12 @@ public void print() { //print board at any moment
 public void reset() { 
 	for (int i=0; i<this.SIZE; i++)
 		for (int j=0; j<this.SIZE; j++)
-			this.getCardboard()[i][j] = 'o';
+			this.getScrabble()[i][j] = 'o';
 }
 
 
 public void drawScrabble() { //invoke this method to draw a valid Scrabble board at once
-this.draw_all_V(7);
+this.draw_all_V(this.middle());
 this.draw_X_as_VV();
 this.draw_scattered_2L();
 this.draw_scattered_3W();
@@ -53,30 +58,24 @@ private int middle(int size) //middle of V pattern- absolut here equals 0, it's 
 
 
 
-public void draw_X_as_VV() { 
-	draw_V_right();
-	draw_V_left();
-}
 
-private void draw_V_left() { //part of drawing X
+private void draw_X_as_VV() { //part of drawing X. X, ><, as > and <. Draws from up 0 row, down to bottom row, by going left>-right<-left>-right<-left>-right<....
 	int j;
 	int absolut;
-	for (int i=0+1; i<this.SIZE-1; i++)
+	for (int i=0+1; i<this.SIZE-1; i++) //Vs don't 'touch' edges of Board
 	{	absolut= java.lang.Math.abs(i-this.middle(this.SIZE));
-		j=this.middle(this.SIZE)- absolut;
-		
-			this.getCardboard()[i][j] = this.assign_type_X(absolut);}
-}
-
-private void draw_V_right() { //part of drawing X
-	int j;
-	int absolut; 
-	for (int i=0+1; i<this.SIZE-1; i++){
-			absolut= java.lang.Math.abs(i-this.middle(this.SIZE));
-			j=this.SIZE-1-(this.middle(this.SIZE)-absolut);
-
-			this.getCardboard()[i][j] = this.assign_type_X(absolut); }
+		if (absolut==0) continue; //double assigning problem at square(middle(),middle()) solved by omitting it 
 	
+		//left half of the X,  V-shape. Draw >
+		j=this.middle(this.SIZE)- absolut;	
+		this.getScrabble()[j][i] = this.assign_type_V(absolut);
+		
+		//right half of the X, of V-shape. Draw <
+		j=this.SIZE-1-j;
+		this.getScrabble()[j][i] = this.assign_type_V(absolut);
+		
+	
+	}
 }
 
 
@@ -87,12 +86,14 @@ public void draw_all_V (int size) {
 	int absolut;
 	for (int i=(this.SIZE-size)/2+1; i<(size+this.SIZE)/2-1; i++)
 		{absolut = java.lang.Math.abs(i-(this.SIZE-size)/2-this.middle(size));
+		
 		j=this.middle(size)-absolut;
-			this.getCardboard()[i][j] = this.assign_type_V(absolut); //left
-			this.getCardboard()[j][i] = this.assign_type_V(absolut); //up
-		j=this.SIZE-1-(this.middle(size)-absolut);
-			this.getCardboard()[i][j] = this.assign_type_V(absolut); //right
-			this.getCardboard()[j][i] = this.assign_type_V(absolut); //down
+		  this.getScrabble()[i][j] = this.assign_type_V(absolut); //left
+		  this.getScrabble()[j][i]= this.assign_type_V(absolut); //up
+		
+		  j=this.SIZE-1-j;
+		  this.getScrabble()[i][j] = this.assign_type_V(absolut); //right
+		  this.getScrabble()[j][i] = this.assign_type_V(absolut); //down
 }
 
 }
@@ -100,34 +101,25 @@ public void draw_all_V (int size) {
 public void draw_scattered_3W () { //scattered squares "multiply word score by 3" 
 	for (int i=0; i<this.SIZE; i+=this.middle())
 		for (int j=0; j<this.SIZE; j+=this.middle())
-			{ if (i==j && i==this.middle())
-				continue; //covered by assign_type_X
-			  else
-				this.getCardboard()[i][j] = this.assign_type_scattered_3W();}
+				  this.getScrabble()[i][j]  = this.assign_type_scattered_3W(i,j);}
 	
 	
-}
+
 
 public void draw_scattered_2L () { //scattered squares "multiply letter score by 2" 
-	int [] I = {0,14};
-	int [] J = {3,this.SIZE-1-3};
-		for (int i: I)
+	int [] I = {0,this.SIZE-1}; //first [] => up and down border of Board, OR second [] => left and right border 
+	int [] J = {this.middle(this.middle()),I[1]-this.middle(this.middle())}; //3 positions away from left and right borders
+		for (int i: I) 
 		for (int j: J)
-		{this.getCardboard()[i][j] = this.assign_type_scattered_2L();
-		this.getCardboard()[j][i] = this.assign_type_scattered_2L();}
+		{this.getScrabble()[i][j]  = this.assign_type_scattered_2L();
+		this.getScrabble()[j][i] = this.assign_type_scattered_2L();}
 }
 
-private char assign_type_X(int absolut) 
-{ 
-	//absolut for X: 6543210123456 and 1 more mirror 
-	if (absolut==0)
-		return 'B';
-	else 
-		return this.assign_type_V(absolut);
-}
+
 
 private char assign_type_V(int absolut) {
-	//absolute for V: 21012 and 3 more mirrors
+	//absolut for small V-pattern: 21012 and 3 more mirrors
+	//absolut for X-pattern: 654321123456 and 1 mirror //no 0, because double assigning conflict at 0
 
 	
 	if (absolut<2)
@@ -139,18 +131,39 @@ private char assign_type_V(int absolut) {
 	
 }
 
-private char assign_type_scattered_3W()//A = TRIPLE WORD SCORE
-{return 'A'; }
+private char assign_type_scattered_3W(int position_x, int position_y)//A = TRIPLE WORD SCORE, B= DOUBLE LETTER SCORE
+{
+	if (position_x==this.middle() && position_y==this.middle())
+		return 'B';
+	else
+		return 'A'; }
 
 private char assign_type_scattered_2L () 	//D= DOUBLE LETTER SCORE
 { return 'D'; }
 
-public char [][] getCardboard() {
-	return cardboard;
+
+
+public char [][] getScrabble() {
+	return scrabble;
 }
 
-public void setCardboard(char [][] cardboard) {
-	this.cardboard = cardboard;
+public void setScrabble(char [][] scrabble) {
+	this.scrabble = scrabble;
 }
 
 }
+
+/*absolut value algorithm is derived as set of transformations of f(x) = |x| function:
+|x-7|, to return only positive indices within scope of scrabble board, size = 15. If size=?, then|x-middle()|
+ 7-|x-7|, to start drawing V from 0, that is from edge of board.  
+ middle()-|x-middle()| 
+Exclude i=0, i=SIZE-1 from the 'for-loop'to not include Squares that are on edge of board
+Exclude Square with absolut=0 from drawing X pattern by two V patterns- left and right. Reason: value assigned two times. Different value to rule of V assignment. If removed from rules, V and X assignment rules are identical. Middle X assignment belongs to draw_Scttered_3W. 
+middle(size)-|x+VECTOR-middle(size)| For small V: add displacement vector, so that's middle(size) is at middle(). So that small V is in the middle, and not drawn from index 0.
+VECTOR OF DISPLACEMENT: start drawing at egde of board at (SIZE-size)/2. 
+	for small v= (15-7)/2 = 3 mod.1 = 3 (drop modulo).
+	for big V = (15-15)/0 = 0 ( X goes across the corners, thus 0)
+Divide by 2 because of symmetry of Squares outside of V. If V.size is 7, 15-7=8 squares outside of V. 4 squares each side. Counted from index 0.  So using V-start vector ends up with result 3.
+END OF VECTOR OF DISPLACEMENT= beginning+length = (SIZE-size)/2 + size = SIZE/2 +size/2 = (size+SIZE)/2
+^in practice, FROM (size-SIZE)/2-1 TO (size+SIZE)/2-1, because ends of V were trimmed, so that they don't touch edge of board 
+*/
