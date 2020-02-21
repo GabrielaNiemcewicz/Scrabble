caa96878b5c849cdbaa8fedd514d35d636f14436
Adam Caffrey <adamca99@gmail.com>
2020-02-21 10:07:22 +0000

    *

--- Square.java
+++ Square.java
@@ -1,46 +1,44 @@
 
 public class Square {
 
-private int TILE_MULTIPLIER;
-private int WORD_MULTIPLIER;
-private Tile tile; 
-
-Square(){
-	this.TILE_MULTIPLIER = 1;
-	this.WORD_MULTIPLIER = 1;
+	private int TILE_MULTIPLIER;
+	private int WORD_MULTIPLIER;
+	private Tile tile; 
 	
-}
-
-public void placeTile(Tile tile){
-	if (this.isEmpty())	
-		this.tile = tile;
-	//else throw new Exception("Error! There's already a Tile placed on the Square...");
-}
-
-public char getCharacter() {
-	//if(this.tile == null) {//use isEmpty() if check needed; I THINK not needed- it may return NULL if object is NULL => desired property!!
-		return tile.getLetter(); 
-		//else return ''; //see later whether Exception or NULL is more useful
+	Square(){
+		this.TILE_MULTIPLIER = 1;
+		this.WORD_MULTIPLIER = 1;
+	}
+	
+	public void placeTile(Tile tile){
+		if (this.isEmpty())	
+			this.tile = tile;
+		//else throw new Exception("Error! There's already a Tile placed on the Square...");
 	}
 
-
-public int getSinglePlacementScore() {
-	if((this.isEmpty())) 
-	return 0; //CHECK LATER: isn't checking Score of empty Square *ALWAYS* indicative of ERROR? => Exception  	
-	else return this.TILE_MULTIPLIER*this.tile.getValue();
-}
-
-public int getWordScoreMultiplier() {
-	return this.WORD_MULTIPLIER ;
-}
-
-public boolean isEmpty() {
-	return  this.tile == null ? true : false;
-
-}
-
-public void removeTile(){
-	this.tile = null;
-}
+	public void removeTile(){
+		this.tile = null;
+	}
+	
+	public char getCharacter() {
+		if(tile == null)
+			return ' ';
+		else
+			return tile.getLetter(); 
+	}
+	
+	public int getSinglePlacementScore() {
+		if((this.isEmpty())) 
+		return 0; //CHECK LATER: isn't checking Score of empty Square *ALWAYS* indicative of ERROR? => Exception  	
+		else return this.TILE_MULTIPLIER*this.tile.getValue();
+	}
+	
+	public int getWordScoreMultiplier() {
+		return this.WORD_MULTIPLIER ;
+	}
+	
+	public boolean isEmpty() {
+		return  this.tile == null ? true : false;
+	}
 
 }
