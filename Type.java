7f9580486f19b61c542cf453f0da13052f826d44
Adam Caffrey <adamca99@gmail.com>
2020-02-23 15:09:57 +0000

    Adding more functionality to type

--- Type.java
+++ Type.java
@@ -1,21 +1,27 @@
 
 public enum Type {
-	normal(1, "  "),
-	doubleWord(2, "2W"),
-	tripleWord(3, "3W"),
-	doubleLetter(2, "2L"),
-	tripleLetter(3, "3L");
+	normal(1, 1, "  "),
+	doubleWord(1, 2, "2W"),
+	tripleWord(1, 3, "3W"),
+	doubleLetter(2, 1, "2L"),
+	tripleLetter(3, 1, "3L");
 	
-	private final int multiplier;
+	private final int WordMultiplier;
+	private final int LetterMultiplier;
 	private final String identifier;
 	
-	Type(int multiplier, String identifier){
-		this.multiplier = multiplier;
+	Type(int LetterMultiplier, int WordMultiplier, String identifier){
+		this.LetterMultiplier = LetterMultiplier;
+		this.WordMultiplier = WordMultiplier;
 		this.identifier = identifier;
 	}
 	
-	public int getMultiplier() {
-		return multiplier;
+	public int getWordMultiplier() {
+		return WordMultiplier;
+	}
+	
+	public int getLetterMultiplier() {
+		return LetterMultiplier;
 	}
 	
 	public String toString() {
