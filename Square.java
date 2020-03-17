0bb4c62c7b099095b63e4cdaf4ea223e88bc3d58
Adam Caffrey <adamca99@gmail.com>
2020-03-17 11:04:10 +0000

    Creating a board with Square class

--- Square.java
+++ Square.java
@@ -1,18 +1,40 @@
 package Jabba;
 
-public class Square {
+import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
+public class Square extends StackPane {
+	static boolean selected;
 	private Tile tile;
 	private Type type;
-	
+	Rectangle r = new Rectangle(40, 40);
+	Text text = new Text();
+
 	Square(){
 		setType(Type.NORMAL);
+		setStyle();
 	}
-	
+
+	private void setStyle(){
+		r.setFill(type.getColour());
+		r.setStroke(Color.BLACK);
+		text.setFont(Font.font("verdana", FontWeight.BOLD, 14));
+		text.setFill(Color.WHITE);
+		setAlignment(Pos.CENTER);
+		getChildren().addAll(r, text);
+	}
+
 	public void setType(Type type) {
 		this.type = type;
+		r.setFill(type.getColour());
+		text.setText(type.toString());
 	}
-	
 	public void placeTile(Tile tile){
 		if (this.isEmpty())	
 			this.tile = tile;
