2fbd087c944466284d1941b95453416448a99dc2
Adam Caffrey <adamca99@gmail.com>
2020-03-17 11:13:08 +0000

    Making a Frame display

--- Tile.java
+++ Tile.java
@@ -1,37 +1,55 @@
 package Jabba;
 
-public class Tile {
+import javafx.geometry.Pos;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
-	    private static final int BLANK_VALUE = 0;
-	    private static final int[] TILE_VALUE = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+public class Tile extends StackPane {
 
-	    private boolean blank;
-	    private char letter;
-	    private int value;
+	private static final int BLANK_VALUE = 0;
+	private static final int[] TILE_VALUE = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
 
-	    // Tile precondition: must be Uppercase letter
-	    Tile(char letter) {
-	        if (letter == '_' || letter==' ') {
-	            this.blank = true;
-	            this.value = BLANK_VALUE;
-	        } 
-	        
-	        else {
-	        	this.blank = false;
-	            
-	             if(letter >= 'a' && letter <= 'z') // This if statement checks for lowerCase characters, 
-	            	letter = (char) (letter - 32);  // Converts LowerCase to UpperCase.
-	            if (letter >= 'A' && letter <= 'Z')
-	            	this.letter = letter;
-	            else 
-	            	throw new IllegalArgumentException();
-	           
-	             this.value = TILE_VALUE[(int) letter - ((int) 'A')];
-	        }
-	    }
-	    
-	    
-	    public boolean isBlank() {
+	private boolean blank;
+	private char letter;
+	private int value;
+
+	// Tile precondition: must be Uppercase letter
+	Tile(char letter) {
+		if (letter == '_' || letter==' ') {
+			this.blank = true;
+			this.value = BLANK_VALUE;
+		}
+
+		else {
+			this.blank = false;
+
+			if(letter >= 'a' && letter <= 'z') // This if statement checks for lowerCase characters,
+				letter = (char) (letter - 32);  // Converts LowerCase to UpperCase.
+			if (letter >= 'A' && letter <= 'Z')
+				this.letter = letter;
+			else
+				throw new IllegalArgumentException();
+
+			this.value = TILE_VALUE[(int) letter - ((int) 'A')];
+		}
+
+		Rectangle r = new Rectangle(40, 60);
+		Text text = new Text(letter + "\n" + value);
+		r.setFill(Color.valueOf("FFE98D"));
+		r.setStroke(Color.BLACK);
+		r.setArcHeight(10);
+		r.setArcWidth(10);
+		text.setFont(Font.font("verdana", FontWeight.BOLD, 14));
+		text.setFill(Color.WHITE);
+		setAlignment(Pos.CENTER);
+		getChildren().addAll(r, text);
+	}
+
+	public boolean isBlank() {
 	        return blank;
 	    }
 
