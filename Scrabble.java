1dd01ace074b1a64dd6d55d1a653196d38e00dc2
nagham aljarad <nagham.aljarad@ucdconnect.ie>
2020-03-24 17:29:43 +0000

    Updating Class Scrabble, Deleting FrameTest

--- Scrabble.java
+++ Scrabble.java
@@ -20,26 +20,35 @@ public class Scrabble extends Application implements EventHandler<ActionEvent> {
     @Override
     public void start(Stage stage) throws Exception {
         window = stage;
+        window.setTitle("Scrabble");
+
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setHgap(10);
+        grid.setVgap(8);
+
         // Player's names:
         player1 = new Label("Player 1: Name: ");
+        GridPane.setConstraints(player1, 1, 5);
         //Player's input
         playerName1 = new TextField();
-
+        GridPane.setConstraints(playerName1, 1, 6);
         // Player's names:
         player2 = new Label("Player 2: Name: ");
+        GridPane.setConstraints(player2, 3, 5);
         //Player's input
         playerName2 = new TextField();
+        GridPane.setConstraints(playerName2, 3, 6);
         //button
-        play = new Button();
-        play.setText("Lets Play");
+        play = new Button("Lets Play");
+        GridPane.setConstraints(play, 2, 9 );
         play.setOnAction(this);
-        help = new Button();
-        help.setText("Help");
-
-        StackPane layout = new StackPane();
-        Scene scene = new Scene(layout, 300, 200);
+        help = new Button("Help");
+        GridPane.setConstraints(help, 1, 9 );
+        help.setOnAction(this);
+        grid.getChildren().addAll(player1, playerName1, player2, playerName2, play, help);
+        Scene scene = new Scene(grid, 500, 200);
         window.setScene(scene);
-
         window.show();
     }
 
@@ -48,7 +57,7 @@ public class Scrabble extends Application implements EventHandler<ActionEvent> {
         if(actionEvent.getSource() == play) {
             Player one = new Player(playerName1.getText());
             Player two = new Player(playerName2.getText());
-            UI.launch();
+          //  UI.launch();
         }
         else if(actionEvent.getSource() == help){
             System.out.println("HELP"); //extra functionality- a text, typed command 'help' required in specification
